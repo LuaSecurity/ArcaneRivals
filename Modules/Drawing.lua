d09026a196e06267c8e36ec577bed06abc3b1538
LuaSecurity <211252621+LuaSecurity@users.noreply.github.com>
2026-01-10 02:50:06 -0300

    Update Drawing.lua

--- Modules/Drawing.lua
+++ Modules/Drawing.lua
@@ -1,23 +1,28 @@
 local DrawingAPI = {}
 DrawingAPI.__index = DrawingAPI
 
-local NewDrawing = Drawing.new
+local NewDrawing = Drawing and Drawing.new or function() return nil end
 local Insert = table.insert
 
 function DrawingAPI.new()
     local self = setmetatable({}, DrawingAPI)
     self.Cache = {}
-    self.Camera = workspace.CurrentCamera
     return self
 end
 
 function DrawingAPI:_Create(type, properties)
     local obj = NewDrawing(type)
+    
+    if not obj then return nil end
+
     if properties then
         for prop, val in pairs(properties) do
-            obj[prop] = val
+            pcall(function()
+                obj[prop] = val
+            end)
         end
     end
+    
     Insert(self.Cache, obj)
     return obj
 end
@@ -27,6 +32,7 @@ function DrawingAPI:Circle(p) return self:_Create("Circle", p) end
 function DrawingAPI:Square(p) return self:_Create("Square", p) end
 function DrawingAPI:Text(p) return self:_Create("Text", p) end
 function DrawingAPI:Triangle(p) return self:_Create("Triangle", p) end
+function DrawingAPI:Quad(p) return self:_Create("Quad", p) end
 
 function DrawingAPI:RemoveAll()
     for i = #self.Cache, 1, -1 do
